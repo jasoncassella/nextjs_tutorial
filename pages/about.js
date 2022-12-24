91482f4cc0fd9da0314ab183873da0488a080a25
Jason Cassella <jasoncassella1@gmail.com>
2022-12-24 12:33:43 -0500

    next js is the goat

--- pages/about.js
+++ pages/about.js
@@ -1,33 +1,29 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-import Link from 'next/link';
 
-export default function Home() {
+const About = () => {
 	return (
 		<>
 			<Head>
-				<title>Ninja List | Home</title>
+				<title>Ninja List | About</title>
 				<meta name='keywords' content='ninjas' />
 			</Head>
 			<div>
-				<h1 className={styles.title}>Homepage</h1>
-				<p className={styles.text}>
+				<h1>About</h1>
+				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias nobis
 					maiores et vel provident dolore esse ipsum iusto reiciendis. Aspernatur,
 					nihil nemo quo temporibus explicabo facere fugit necessitatibus esse
 					sapiente.
 				</p>
-				<p className={styles.text}>
+				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias nobis
 					maiores et vel provident dolore esse ipsum iusto reiciendis. Aspernatur,
 					nihil nemo quo temporibus explicabo facere fugit necessitatibus esse
 					sapiente.
 				</p>
-				<Link href='/ninjas' className={styles.btn}>
-					See Ninja Listing
-				</Link>
 			</div>
 		</>
 	);
-}
+};
+
+export default About;
